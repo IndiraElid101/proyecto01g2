0485f60284b2b04b76782de84b0a6449e4bf3d5a
IndiraElid101 <118759206+IndiraElid101@users.noreply.github.com>
2022-11-21 12:46:21 -0500

    Add files via upload

---
+++ js/ejemplo4.js
@@ -0,0 +1,20 @@
+/* Estructuras repetitivas
+    FOR → Cuando sabes cuantas veces se va arepetor in proceso
+    WHILE → Cuando se desconoce las veces que se repetirá el proceso, 
+           ( Ya que la repetición del proceso está condicionado)
+
+
+// Ejemplo mostrar 10 veces el mensaje hola
+//Crear un ciclo repetitivo numerico → Inicio hasta final → 1 hasta 10 (No mas de 10)
+*/
+ for (let i = 1; i<=10; i++){
+    alert("Hola" + 1);
+ }
+
+
+let op = "";
+while(true){
+    alert("Hola");
+    op = prompot("Desea continuar")
+}
+<p></p>
